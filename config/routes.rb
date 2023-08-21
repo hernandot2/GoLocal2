849321cc535123d4d46700eccb24aa7ab4b2c55c
3por5 <73372271+3por5@users.noreply.github.com>
2023-08-20 21:33:42 -0300

    pre-devise-edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords'}
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
