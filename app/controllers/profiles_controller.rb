b64c7eb3be877425acddf1ea5ca2c8ce39cd7988
Julia Artigas <juju_artigas@hotmail.com>
2023-08-29 22:14:53 -0300

    criada a página events#index

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,14 +1,15 @@
 class ProfilesController < ApplicationController
+before_action :set_user, only: [:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
+    @user_locations = @user.favorites
+    @user_events = @user.events
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to profile_path(@user), notice: 'Dados atualizados com sucesso!'
     else
@@ -17,6 +18,10 @@ class ProfilesController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:email, :name, :description, :photo)
