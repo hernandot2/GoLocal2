10ab7ff069616bf3e3e1863f2efcf315b6b09a31
Julia Artigas <juju_artigas@hotmail.com>
2023-09-02 12:43:25 -0300

    configs policies de event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,10 +2,11 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def index
-    @events = Event.all
+    @events = policy_scope(Event)
   end
 
   def show
+    authorize @event
   end
 
   def new
