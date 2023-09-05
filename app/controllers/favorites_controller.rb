b54b58f7b0db196e804739b3bd202e6fc066d889
Julia Artigas <juju_artigas@hotmail.com>
2023-09-05 13:58:55 -0300

    added favorite_policy.rb e permissões para liberação de favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,19 +3,21 @@ class FavoritesController < ApplicationController
   before_action :set_favorite, only: [:show, :edit, :update, :destroy]
 
   def index
-    @favorites = current_user.favorites
+    @favorites = policy_scope(Favorite).where(user: current_user)
   end
 
   def create
     @location = Location.find(params[:location_id])
     @favorite = Favorite.new(user: current_user, location: @location)
+    authorize @favorite
     @favorite.save!
     redirect_to favorites_path, notice: 'Localização favoritada com sucesso!'
   end
 
   def destroy
+    authorize @favorite
     @favorite.destroy
-    redirect_to favorites_path, notice: "Favorito excluido com sucesso!"
+    redirect_to favorites_path, notice: 'Favorito excluido com sucesso!'
   end
 
   private
