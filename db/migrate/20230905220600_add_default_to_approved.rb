398c87ce0ce6ac5bca26ec6f0d341fc2b11e7fb2
Julia Artigas <juju_artigas@hotmail.com>
2023-09-07 17:30:46 -0300

    remove migration default approved

--- db/migrate/20230905220600_add_default_to_approved.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultToApproved < ActiveRecord::Migration[7.0]
-  def change
-    # change_column_default :events, :approved, false
-    # change_column_default :locations, :approved, false
-  end
-end
